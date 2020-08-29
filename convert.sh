c7582465992b044b99bc686b7cea899904049952
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-08-29 03:00:21 +0000

    Added code to package models

--- convert.sh
+++ convert.sh
@@ -3,3 +3,9 @@ onmt-main --config config.yml --auto_config average_checkpoints --output_dir ave
 
 # Convert to CTranslate
 ct2-opennmt-tf-converter --model_path averaged_model --model_spec TransformerBase --output_dir converted_model --src_vocab tokenized/vocab.vocab --tgt_vocab tokenized/vocab.vocab --quantization int8
+
+# Create model package
+mkdir packaged_model
+cp -r converted_model packaged_model/
+cp sentencepiece.model packaged_model/
+
