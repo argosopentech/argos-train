480675ec270b855d0795a727bead8440e893f212
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-10-01 19:07:31 -0500

    Fixed bug in reset.sh

--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
-rm -rf sentencepiece.* tokenized/ wmt_ende_transformer/ raw_data/all.txt raw_data/train.* raw_data/valid.*
-./reset_packacking.sh
+rm -rf sentencepiece.* tokenized/ model_dir/ raw_data/all.txt raw_data/train.* raw_data/valid.*
+./reset_packaging.sh
