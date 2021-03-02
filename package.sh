ae5278a7771f3fedff0124971b6c3acc4e0fea53
P.J. Finlay <pjappdevelopment@gmail.com>
2021-03-02 03:22:25 +0000

    Working on packaging scripts

--- package.sh
+++ package.sh
@@ -1,25 +1,33 @@
 source config.sh
 ./reset_packaging.sh
 
-# Convert to CTranslate
-ct2-opennmt-py-converter --model_path averaged.pt --model_spec TransformerBase --output_dir converted_model --quantization int8
+echo "Averaging checkpoints"
+./../OpenNMT-py/tools/average_models.py -m \
+	openmt.model_step_10000.pt \
+	openmt.model_step_11000.pt \
+	-o averaged.pt
+# Debug:
+# cp openmt.model_step_20000.pt averaged.pt
 
-# Create model package
+echo "Converting to CTranslate"
+ct2-opennmt-py-converter --model_path averaged.pt --model_spec TransformerBase --output_dir ctranslate_model --quantization int8
+
+echo "Create model package"
 mkdir packaged_model
-cp -r converted_model packaged_model/model
+cp -r ctranslate_model packaged_model/model
 
-# Copy in sentencepiece
+echo "Copy in sentencepiece"
 cp sentencepiece.model packaged_model/
 
-# Setup Stanza sentence boundary detection
+echo "Setup Stanza sentence boundary detection"
 python3 -c "import stanza; stanza.download('$stanza_lang_code', dir='stanza', processors='tokenize')"
 cp -r stanza packaged_model/
 
-# Copy in metadata.json and MODEL_README.md
+echo "Copy in metadata.json and MODEL_README.md"
 cp metadata.json packaged_model/
 cp MODEL_README.md packaged_model/README.md
 
-# Zip packaged model into Argos Translate model
+echo "Zip packaged model into Argos Translate model"
 mv packaged_model "${sl}_${tl}"
 zip -r "${sl}_${tl}.argosmodel" "${sl}_${tl}"
 
--- reset_packaging.sh
+++ reset_packaging.sh
@@ -1,3 +1,3 @@
 source config.sh
 
-rm -rf exported/ averaged_model/ converted_model/ packaged_model/ *.argosmodel env/ stanza/ "${sl}_${tl}" "checkpoint_export_${sl}_${tl}/" checkpoint_export*.zip
+rm -rf averaged.pt exported/ averaged_model/ ctranslate_model/ packaged_model/ *.argosmodel env/ stanza/ "${sl}_${tl}" "checkpoint_export_${sl}_${tl}/" checkpoint_export*.zip
