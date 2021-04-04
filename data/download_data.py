4e2ba035b39afa3e739dc6dc48805575b4d10f9e
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-04 08:10:53 -0500

    Download and extract data working

--- data/download_data.py
+++ data/download_data.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+from collections import deque
 import json
+import zipfile
+
 
 import requests
 
@@ -21,25 +24,61 @@ class Dataset:
         self.links = metadata.get('links')
 
     def __str__(self):
-        return '-'.join([
-                str(self.name),
-                str(self.type),
-                str(self.from_code),
-                str(self.to_code),
-                str(self.name)])
+        return str(self.name).lower() + '-' + \
+                str(self.from_code) + '_' + \
+                str(self.to_code)
+
+    def filename(self):
+        return str(self) + '.argosmodel'
+
+    def filepath(self):
+        return Path(Dataset.CACHE_PATH) / self.filename()
 
     def download(self):
         """Downloads the package and returns its path"""
         url = self.links[0]
-        filename = str(self) + '.argosdata'
-        filepath = CACHE_PATH / filename
+        filepath = self.filepath()
         if not filepath.exists():
+            print(f'Downloading {filepath}')
             r = requests.get(url, allow_redirects=True)
             open(filepath, 'wb').write(r.content)
         return filepath
 
+    def data(self):
+        """Returns a tuple of source and target data.
+
+        Source and target data is collections.deque
+        """
+        filepath = self.filepath()
+        assert(Path(filepath).exists())
+        assert(zipfile.is_zipfile(filepath))
+        source = None
+        target = None
+        with zipfile.ZipFile(filepath, 'r') as zip_cache:
+            with zip_cache.open(str(self) + '/source', 'r') as source_file:
+                source = deque(source_file.readlines())
+            with zip_cache.open(str(self) + '/target', 'r') as target_file:
+                target = deque(target_file.readlines())
+        assert(source != None)
+        assert(target != None)
+        assert(len(source) == len(target))
+        return (source, target)
 
 DATA_INDEX = Path('index.json')
+available_datasets = []
 with open(DATA_INDEX) as data_index:
     index = json.load(data_index)
-    print(index)
+    for data_metadata in index:
+        dataset = Dataset()
+        dataset.load_metadata_from_json(data_metadata)
+        available_datasets.append(dataset)
+
+# Get Spanish data
+es_data = list(filter(
+        lambda x: x.to_code == 'es',
+        available_datasets))
+for dataset in es_data:
+    dataset.download()
+    data = dataset.data()
+    print(data)
+
