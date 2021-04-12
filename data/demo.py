e69feebde26d0d8e79e0dd8b0b8ff931d3415ad3
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-12 07:55:13 -0500

    Cleaned up data demo code

--- data/demo.py
+++ data/demo.py
@@ -3,12 +3,16 @@
 from argostrain.dataset import *
 from argostrain.sbd import *
 
+# Load data
 input_dataset = FileDataset(open('testdata_source'), open('testdata_target'))
+
+# Generate SBD data
 sbd_dataset = generate_sbd_data(input_dataset)
 # At most use 0.1 as much sbd data as input data
 trimmed_sbd_data = sbd_dataset.data(int(len(input_dataset) * 0.1))
 sbd_dataset = Dataset(trimmed_sbd_data[0], trimmed_sbd_data[1])
 
+# Combine datasets
 dataset = CompositeDataset(input_dataset) + CompositeDataset(sbd_dataset)
 
 
