1fc8342499b3d8a18e505167a75156b007c0c212
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-19 17:03:43 -0500

    Made tokenizer coverage optional

--- config.sh
+++ config.sh
@@ -3,8 +3,10 @@ export tl="zh"
 export stanza_lang_code=$sl
 export vocab_size=32000
 
-# 1 for languages with a small character set
-# 0.9995 for languages like Japanese or Chinese
+############
+# Optional #
+############
+
 export character_coverage=0.9995
 
 # Special Tokens
