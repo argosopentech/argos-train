691693c26bb36d470c375897842b0e8b984d7c44
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-28 22:30:35 +0000

    Moved scripts to /bin

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     packages=find_packages(),
     install_requires=required_packages,
     include_package_data=True,
-    scripts=['bin/argos-generate-data'],
+    scripts=['bin/argos-generate-data', 'bin/argos-train', 'bin/generate-wiktionary-data'],
 )
