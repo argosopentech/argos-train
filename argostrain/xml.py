53be22ca05cc9d44c2bd27bc7f61a74a435141b2
P.J. Finlay <pjappdevelopment@gmail.com>
2021-05-01 00:51:05 +0000

    Tag injection working

--- argostrain/xml.py
+++ argostrain/xml.py
@@ -6,8 +6,10 @@ from collections import deque
 from argostranslate import package, translate
 
 MIN_TAG_TEXT_LENGTH = 10
+OPEN_TOKEN = '<x>'
+CLOSE_TOKEN = '</x>'
 
-def generate_xml_data(dataset, source_code, target_code):
+def generate_xml_data(dataset, source_code, target_code, num):
     xml_source = deque()
     xml_target = deque()
     source, target = dataset.data()
@@ -22,8 +24,14 @@ def generate_xml_data(dataset, source_code, target_code):
     for i in range(len(source)):
         source_line = source[i]
         target_line = target[i]
+        info(f'Processing xml line {i} ' + \
+                'num={num} ' + \
+                'source_line={source_line} ' + \
+                'target_line={target_line}')
         best_source_start_index = None
         best_source_end_index = None
+        best_matching_index = None
+        best_target_end_index = None
         best_score = None
         for source_start_index in range(len(source_line) - 1):
             for source_end_index in range(source_start_index, len(source_line) + 1):
@@ -33,14 +41,30 @@ def generate_xml_data(dataset, source_code, target_code):
                 translation_hypothesis = source_translation.hypotheses(source_sub_string, 1)[0]
                 translated_sub_string = translation_hypothesis.value
                 score = translation_hypothesis.score
-                if translated_sub_string not in target_line:
+                matching_index = target_line.find(translated_sub_string)
+                if matching_index == -1:
                     continue
                 if best_score == None or score > best_score:
                     best_source_start_index = source_start_index
                     best_source_end_index = source_end_index
+                    best_matching_index = matching_index
+                    best_target_end_index = matching_index + len(translated_sub_string)
                     best_score = score
-                    print(source_sub_string)
-                    print(translated_sub_string)
-    return Dataset(['Hello\n'], ['World\n'])
+        if best_score == None:
+            continue
+        xml_source.append(
+                source_line[:best_source_start_index] + \
+                OPEN_TOKEN + \
+                source_line[best_source_start_index:best_source_end_index] + \
+                CLOSE_TOKEN + \
+                source_line[best_source_end_index - 1:])
+        xml_target.append(
+                target_line[:best_matching_index] + \
+                OPEN_TOKEN + \
+                target_line[best_matching_index:best_target_end_index] + \
+                CLOSE_TOKEN + \
+                target_line[best_target_end_index - 1:])
+
+    return Dataset(xml_source, xml_target)
 
     
