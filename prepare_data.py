bd36bf66376e602aad95f1322089a22c16862f1f
P.J. Finlay <pjappdevelopment@gmail.com>
2021-08-15 00:29:52 +0000

    Reordered file writes

--- prepare_data.py
+++ prepare_data.py
@@ -42,26 +42,30 @@ dataset = Dataset(source, target)
 print("Done filtering data")
 
 # Split and write data
+VALID_SIZE = 2000
+assert(len(source_data) > VALID_SIZE)
+
 source_data, target_data = dataset.data()
 source_data = list(source_data)
 target_data = list(target_data)
 
 os.mkdir('run/split_data')
+
 source_valid_file = open('run/split_data/src-val.txt', 'w')
-source_train_file = open('run/split_data/src-train.txt', 'w')
-target_valid_file = open('run/split_data/tgt-val.txt', 'w')
-target_train_file = open('run/split_data/tgt-train.txt', 'w')
-
-VALID_SIZE = 2000
-assert(len(source_data) > VALID_SIZE)
 source_valid_file.writelines(source_data[0:VALID_SIZE])
-source_train_file.writelines(source_data[VALID_SIZE:])
-target_valid_file.writelines(target_data[0:VALID_SIZE])
-target_train_file.writelines(target_data[VALID_SIZE:])
-
 source_valid_file.close()
+
+source_train_file = open('run/split_data/src-train.txt', 'w')
+source_train_file.writelines(source_data[VALID_SIZE:])
 source_train_file.close()
+
+target_valid_file = open('run/split_data/tgt-val.txt', 'w')
+target_valid_file.writelines(target_data[0:VALID_SIZE])
 target_valid_file.close()
+
+target_train_file = open('run/split_data/tgt-train.txt', 'w')
+target_train_file.writelines(target_data[VALID_SIZE:])
 target_train_file.close()
+
 print('Done splitting data')
 
