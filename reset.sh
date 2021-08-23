f473ffce13fa9e0c0ecb3bbf3cdf421d8a354c0e
P.J. Finlay <pjappdevelopment@gmail.com>
2021-08-23 17:27:20 -0500

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
-rm -rf sentencepiece.* split_data/ openmt.* opennmt_data raw_data/wiktionary.* raw_data/source raw_data/target
+rm -rf sentencepiece.* run/ openmt.* opennmt_data raw_data/wiktionary.* raw_data/source raw_data/target
 ./reset_packaging.sh
