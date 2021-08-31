88239b0664045066e159863e811c129ef7e85384
P.J. Finlay <pjappdevelopment@gmail.com>
2021-08-31 12:19:00 +0000

    Installing dependencies

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -1,16 +1,27 @@
-
 # Install Debian packages
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install python3 python3-pip python3-virtualenv zip -y
+sudo apt-get install python3 python3-pip python3-virtualenv zip git -y
 
-# Install Python
-cd ~/onmt-models
-python3 -m pip install -e .
+# Setup env
+echo "DEBIAN_FRONTEND=noninteractive" >> ~/.profile
+echo 'PATH=$PATH:~/.local/bin:~/env/bin' >> ~/.profile
+source ~/.profile
+
+# Setup Python environment
+virtualenv ~/env
+source env/bin/activate
+git clone https://github.com/argosopentech/onmt-models.git ~/onmt-models
+~/env/bin/pip install -e ~/onmt-models
+
+# Install OpenNMT-py
+git clone https://github.com/OpenNMT/OpenNMT-py.git ~/OpenNMT-py
+~/env/bin/pip install -e ~/OpenNMT-py
+~/env/bin/pip install -r ~/OpenNMT-py/requirements.opt.txt
 
 # Compile SentencePiece
 cd
 sudo apt-get install cmake build-essential pkg-config libgoogle-perftools-dev vim git -y
-git clone https://github.com/google/sentencepiece.git
+git clone https://github.com/google/sentencepiece.git ~/sentencepiece
 cd sentencepiece
 mkdir build
 cd build
@@ -18,6 +29,5 @@ cmake ..
 make -j $(nproc)
 sudo make install
 sudo ldconfig -v
-
 
 
