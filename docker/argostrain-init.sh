a1b746490d7b1c56944a3853001130f8fbd490cf
P.J. Finlay <pjappdevelopment@gmail.com>
2021-09-05 13:43:37 +0000

    Added htop

--- docker/argostrain-init.sh
+++ docker/argostrain-init.sh
@@ -1,6 +1,6 @@
 # Install Debian packages
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install python3 python3-pip python3-virtualenv zip git wget curl screen -y
+sudo apt-get install python3 python3-pip python3-virtualenv zip git wget curl screen htop -y
 
 # Setup Python environment
 virtualenv ~/env
