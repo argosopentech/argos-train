254298d534a45d8ce395791b2fb4599de39603ed
P.J. Finlay <pjappdevelopment@gmail.com>
2022-09-26 06:59:39 -0500

    Fixed chunk data script

--- scripts/generate_chunk_data.py
+++ scripts/generate_chunk_data.py
@@ -30,22 +30,18 @@ for dataset in datasets:
     generated_target = list()
     source_data, target_data = dataset.data()
     for i in range(0, len(source_data) - 1):
-        first_sentence = source_data[i].strip()
-        second_sentence = source_data[i + 1].strip()
+        first_sentence = str(source_data[i]).strip()
+        second_sentence = str(source_data[i + 1]).strip()
 
         # Randomly split second sentence
         second_sentence = second_sentence[0 : random.randint(0, len(second_sentence))]
 
-        # Space pad end of first sentence
-        if len(first_sentence) > 0:
-            if first_sentence[-1] != " ":
-                first_sentence = first_sentence + " "
-
-        generated_source.append(first_sentence + second_sentence)
-        generated_target.append(first_sentence)
+        generated_source.append(first_sentence + " " + second_sentence + "\n")
+        generated_target.append(first_sentence + "\n")
 
     # Write generated data to file
     with open(source_file_path, "a") as source_file:
         source_file.writelines(generated_source)
     with open(target_file_path, "a") as target_file:
         target_file.writelines(generated_target)
+    print("Wrote generated chunk data")
